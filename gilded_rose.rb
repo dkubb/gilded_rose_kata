54579e5e04a93dbf5a055d0a1d7f8a0777674117
Dan Kubb <github@dan.kubb.ca>
2020-06-05 09:22:48 -0700

    Refactor ItemUpdater#call so backstage pass conditional is simpler

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -11,9 +11,8 @@ module GildedRose
         self.quality += 1
         if name == 'Backstage passes to a TAFKAL80ETC concert'
           if sell_in < 6
-            self.quality += 1
-          end
-          if sell_in < 11
+            self.quality += 2
+          elsif sell_in < 11
             self.quality += 1
           end
         end
